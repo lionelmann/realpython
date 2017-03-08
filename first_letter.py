3e601ed5e140cbb7e7404ba2bda62bd783a2ff0c
Lionel Mann <lionelmann@gmail.com>
2017-03-08 13:03:16 -0500

    assignment 1

---
+++ first_letter.py
@@ -0,0 +1,8 @@
+"""Script should determine the first letter of the user's input \
+convert it to uppercase and display it back"""
+
+password = input("Tell me your password")
+
+response = password.upper()
+
+print(response[0])
